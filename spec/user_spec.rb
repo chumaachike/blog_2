4220b6fc38b75b8ceb6c0c8d7d848c4dd417922c
wayungi <fwayungi@gmail.com>
2022-11-08 10:01:28 +0300

    add valid user test

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  user = User.new(name: 'Anyone')
+  it 'is valid with valid attributes' do
+    expect(user).to be_valid
+  end
+
 end
